b39dac61f46e76b00934bc1206512e837c3a7a81
Ben Dang <s224116668@deakin.edu.au>
2024-07-18 20:51:20 +1000

    add

--- example-3/src/index.js
+++ example-3/src/index.js
@@ -1,11 +1,13 @@
 const express = require("express");
 const fs = require("fs");
-
+require("dotenv").config();
 //
 // Throws an error if the PORT environment variable is missing.
 //
 if (!process.env.PORT) {
-    throw new Error("Please specify the port number for the HTTP server with the environment variable PORT.");
+  throw new Error(
+    "Please specify the port number for the HTTP server with the environment variable PORT."
+  );
 }
 
 //
@@ -19,19 +21,21 @@ const app = express();
 // Registers a HTTP GET route for video streaming.
 //
 app.get("/video", async (req, res) => {
-    const videoPath = "../videos/SampleVideo_1280x720_1mb.mp4";
-    const stats = await fs.promises.stat(videoPath);
+  const videoPath = "../videos/SampleVideo_1280x720_1mb.mp4";
+  const stats = await fs.promises.stat(videoPath);
 
-    res.writeHead(200, {
-        "Content-Length": stats.size,
-        "Content-Type": "video/mp4",
-    });
-    fs.createReadStream(videoPath).pipe(res);
+  res.writeHead(200, {
+    "Content-Length": stats.size,
+    "Content-Type": "video/mp4",
+  });
+  fs.createReadStream(videoPath).pipe(res);
 });
 
 //
 // Starts the HTTP server.
 //
 app.listen(PORT, () => {
-    console.log(`Microservice listening on port ${PORT}, point your browser at http://localhost:${PORT}/video`);
+  console.log(
+    `Microservice listening on port ${PORT}, point your browser at http://localhost:${PORT}/video`
+  );
 });
